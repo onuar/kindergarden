ebe4c7e6495095dbfa75d760fc86196f2fd7c46e
ozgend <revolter00@gmail.com>
2016-12-22 13:32:43 +0300

    initialized werkzeugs host

--- werkzeugs-garden/app.py
+++ werkzeugs-garden/app.py
@@ -1,10 +1,30 @@
+import json
+import requests
 from werkzeug.wrappers import Request, Response
+
 
 @Request.application
 def application(request):
-    print(request)
-    return Response('Hello World!')
+    print(request.full_path)
+
+    with open('routing-map.json') as json_data:
+        routing_map = json.load(json_data)
+
+    print(routing_map["nodes"])
+
+    
+
+    response = Response('rıytşb')
+    return response
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('localhost', 8000, application)
+
+
+class Nodes(object):
+    order int
+    url str
+    def __init__(self, url):
+        self.name = name
+        self.username = username
