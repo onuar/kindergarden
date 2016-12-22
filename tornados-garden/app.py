fc17a5822cba2497425dea28406d7eb13bb18cd3
Onur Aykac <onuraykac@gmail.com>
2016-12-22 16:55:42 +0200

    tornado - dispatch get requests

--- tornados-garden/app.py
+++ tornados-garden/app.py
@@ -1,5 +1,7 @@
 from tornado.web import RequestHandler, Application
 import tornado.ioloop
+import requests
+import json
 
 
 class MainHandler(RequestHandler):
@@ -8,13 +10,27 @@ class MainHandler(RequestHandler):
 
 
 class ApiHandler(RequestHandler):
+    last_node_index = 0
+    nodes = {}
+
+    def initialize(self):
+        # nonlocal nodes
+        # nonlocal last_node_index
+
+        with open("routing-map-tor.json", "r") as config_file:
+            self.nodes = json.load(config_file)
+
     def get(self):
         self._logging()
+        response = self._dispatch("GET")
+
         self.write("hello api")
 
     def post(self):
         self._logging()
         response_json = {'name':'onur', 'surname':'aykac'}
+
+        response = self._dispatch("POST")
         self.write({'data':response_json})
 
     def _logging(self):
@@ -24,8 +40,16 @@ class ApiHandler(RequestHandler):
         print(">>> Body: " + str(self.request.body))
         print("---------")
 
-    def _internal_call(self):
-        pass
+    def _dispatch(self, method):
+        url = self.nodes["nodes"][self.last_node_index]
+        internal_response = requests.request(method=method, url=url)
+
+        self.last_node_index += 1
+
+        if self.last_node_index >= self.node["nodes"].length:
+            self.last_node_index = 0
+
+        return internal_response
 
 
 def make_app():
