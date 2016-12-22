4c62d2ab0b58ee3a73e446fbfc431e3765283fb1
ozgend <revolter00@gmail.com>
2016-12-22 20:14:41 +0300

    intro tu forward and retreive http request

--- werkzeugs-garden/app.py
+++ werkzeugs-garden/app.py
@@ -2,29 +2,50 @@ import json
 import requests
 from werkzeug.wrappers import Request, Response
 
+routing_map = {}
+last_route_index = -1
+
 
 @Request.application
 def application(request):
     print(request.full_path)
+    # print(routing_map["nodes"])
 
+    forward_url = get_forward_url(request.full_path)
+    print("route -> ", forward_url)
+
+    f_response = requests.request(
+        request.method, forward_url, data=request.data, headers=request.headers)
+
+    response = Response(f_response.content)
+    response.headers.clear()
+
+    for key, value in f_response.headers.items():
+        response.headers[key] = value
+
+    return response
+
+
+def get_next_route_index():
+    global last_route_index
+    length = len(routing_map["nodes"])
+    next_route_index = last_route_index + 1
+
+    if next_route_index == length:
+        next_route_index = 0
+
+    last_route_index = next_route_index
+    return next_route_index
+
+
+def get_forward_url(request_path):
+    next_route_index = get_next_route_index()
+    return routing_map["nodes"][next_route_index] + "" + request_path
+
+
+if __name__ == '__main__':
     with open('routing-map.json') as json_data:
         routing_map = json.load(json_data)
 
-    print(routing_map["nodes"])
-
-    
-
-    response = Response('rıytşb')
-    return response
-
-if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('localhost', 8000, application)
-
-
-class Nodes(object):
-    order int
-    url str
-    def __init__(self, url):
-        self.name = name
-        self.username = username
