45d81213da0ae96123b093427ea5acb11de33dfb
ozgend <revolter00@gmail.com>
2016-12-23 16:45:07 +0300

    werkzeug - changed to http.client.request

--- werkzeugs-garden/app.py
+++ werkzeugs-garden/app.py
@@ -1,5 +1,8 @@
+import http.client
+import re
+import urllib
+import urllib.parse
 import json
-import requests
 from werkzeug.wrappers import Request, Response
 
 routing_map = {}
@@ -8,23 +11,52 @@ last_route_index = -1
 
 @Request.application
 def application(request):
-    print(request.full_path)
-    # print(routing_map["nodes"])
 
-    forward_url = get_forward_url(request.full_path)
-    print("route -> ", forward_url)
+    # f_url = get_forward_url(request.full_path)
+    f_url = "localhost:5001"
+    hostname, port = parse_host_port(f_url)
 
-    f_response = requests.request(
-        request.method, forward_url, data=request.data, headers=request.headers)
+    print("route - {} >> {} ".format(request.full_path, f_url))
 
-    response = Response(f_response.content)
+
+    f_request_headers = dict(request.headers)
+
+    if request.method == "POST" or request.method == "PUT":
+        form_data = list(iterform(request.form))
+        form_data = urllib.urlencode(form_data)
+        f_request_headers["Content-Length"] = len(form_data)
+    else:
+        form_data = None
+
+    f_connection = http.client.HTTPConnection(hostname,port)
+    f_connection.request(request.method, request.full_path, body=form_data, headers=f_request_headers)
+    f_response = f_connection.getresponse()
+    
+    content = f_response.read()
+    status = f_response.status
+    content_type = f_response.getheader('content-type')
+    
+    response = Response(content, status=status, mimetype=content_type, content_type=content_type)
     response.headers.clear()
 
-    for key, value in f_response.headers.items():
+    for key, value in f_response.getheaders():
         response.headers[key] = value
 
     return response
 
+
+def iterform(multidict):
+    for key in multidict.keys():
+        for value in multidict.getlist(key):
+            yield (key.encode("utf8"), value.encode("utf8"))
+
+def parse_host_port(h):
+    host_port = h.split(":", 1)
+    if len(host_port) == 1:
+        return (h, 80)
+    else:
+        host_port[1] = int(host_port[1])
+        return host_port
 
 def get_next_route_index():
     global last_route_index
