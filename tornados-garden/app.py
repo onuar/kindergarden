0387e4117dc623e98fe2266faa4afd8da331754c
Onur Aykac <onuraykac@gmail.com>
2017-01-04 15:01:17 +0200

    removed requests and used Tornado Http Client

--- tornados-garden/app.py
+++ tornados-garden/app.py
@@ -1,71 +1,67 @@
-from tornado.web import RequestHandler, Application
 import tornado.ioloop
-import requests
 import json
 
-
-class MainHandler(RequestHandler):
-    def get(self):
-        self.write("This not a LB.");
+from tornado.web import RequestHandler, Application
+from tornado.httpclient import HTTPRequest, AsyncHTTPClient
 
 
-class LBHandler(RequestHandler):
+class EntryPoint(RequestHandler):
     last_node_index = 0
     nodes = {}
 
     def initialize(self):
-        # nonlocal nodes
-        # nonlocal last_node_index
-
         with open("routing-map-tor.json", "r") as config_file:
             self.nodes = json.load(config_file)
 
     def prepare(self):
         print("beginning of the request")
 
-    def get(self, slug):
-        self._logging()
-        response = self._dispatch("GET")
+    @tornado.web.asynchronous
+    def get(self):
+        self.write_log()
+        self.dispatch_internal(response_callback=self.response_handler)
 
-        # for key in self.request.headers.keys():
-        #     self.clear_header(key)
-        #     print("removed header: " + key)
+    def response_handler(self, response):
+        self.set_status(response.code)
+        self._headers = tornado.httputil.HTTPHeaders()
 
-        for key, value in response.headers.items():
-            self.add_header(key, value)
+        for header, v in response.headers.get_all():
+            if header not in ('Content-Length', 'Transfer-Encoding', 'Content-Encoding', 'Connection'):
+                self.add_header(header, v)
 
-        self.write(response.content)
+        if response.body:
+            self.set_header('Content-Length', len(response.body))
+            self.write(response.body)
 
-    def post(self):
-        self._logging()
-        response_json = {'name': 'onur', 'surname': 'aykac'}
+        self.finish()
 
-        response = self._dispatch("POST")
-        self.write({'data': response_json})
+    def dispatch_internal(self, response_callback):
+        url = self.get_node()
+        body = self.request.body
+        if not body:
+            body = None
+        if self.request.path:
+            url = url + self.request.path
 
-    def _logging(self):
-        print(self.request.uri)
-        print(">>> Headers: " + str(self.request.headers._dict))
-        print(">>> Query string parameters: " + str(self.request.arguments))
-        print(">>> Body: " + str(self.request.body))
-        print("---------")
+        request = HTTPRequest(url=url, headers=self.request.headers, method=self.request.method, body=body)
+        client = AsyncHTTPClient()
+        client.fetch(request, response_callback)
 
-    def _dispatch(self, method):
+    def get_node(self):
         url = self.nodes["nodes"][self.last_node_index]
-        internal_response = requests.request(method=method, url=url)
-
         self.last_node_index += 1
 
         if self.last_node_index >= len(self.nodes["nodes"]):
             self.last_node_index = 0
+        return url
 
-        return internal_response
+    def write_log(self):
+        print(self.request.uri)
 
 
 def make_app():
     return Application([
-        # (r"/", MainHandler),
-        (r"/.?", LBHandler)
+        (r'.*', EntryPoint)
     ])
 
 
